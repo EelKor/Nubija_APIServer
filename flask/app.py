4f47b700bfd329c5696ab54d28874e3ef3ec82ad
SeugnshinLee <lsver67@gmail.com>
2021-01-05 22:12:03 +0900

    로그 파일 분활 기능 추가

--- flask/app.py
+++ flask/app.py
@@ -2,13 +2,21 @@ from flask import Flask, jsonify, render_template
 from dbaccess import DBAccess
 import logging
 from logging import Formatter, FileHandler
+from logging import handlers
+
+#로그 파일 설정
+fileMaxByte = 1024 * 1024 * 100
+log_file_count = 20
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 fileHandler = FileHandler('log/flask.log')
 fileHandler.setFormatter(
     Formatter('[%(asctime)s][%(levelname)s|%(filename)s:%(lineno)s] >> %(message)s'))
+fileHandler = logging.handlers.RotatingFileHandler(filename='/log/flask.log', maxBytes= fileMaxByte, backupCount= log_file_count)
 logger.addHandler(fileHandler)
+
+
 
 app = Flask(__name__)
 db = DBAccess()
