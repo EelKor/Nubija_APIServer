1927eb5ba66c958d5ea1291a6116c98012d64bcf
SeugnshinLee <lsver67@gmail.com>
2021-01-05 21:12:08 +0900

    오류 로그 기능 추가

--- flask/app.py
+++ flask/app.py
@@ -1,5 +1,14 @@
 from flask import Flask, jsonify, render_template
 from dbaccess import DBAccess
+import logging
+from logging import Formatter, FileHandler
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+fileHandler = FileHandler('log/flask.log')
+fileHandler.setFormatter(
+    Formatter('[%(asctime)s][%(levelname)s|%(filename)s:%(lineno)s] >> %(message)s'))
+logger.addHandler(fileHandler)
 
 app = Flask(__name__)
 db = DBAccess()
@@ -13,6 +22,16 @@ def terminalinfo(id):
     id = int(id)
     data = db.read(id)
     return jsonify(data[0])
+
+@app.errorhandler(404)
+def page_not_found(error):
+    logger.exception(error)
+
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    logger.exception(error)
 
 
 if __name__ == "__main__":
