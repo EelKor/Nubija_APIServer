12c248cf21d6e9748cae61965f7a265f0fd493a0
SeugnshinLee <lsver67@gmail.com>
2021-01-30 22:48:36 +0900

    주석 추가

--- updater/main.py
+++ updater/main.py
@@ -2,6 +2,7 @@ from dbaccess import DBAccess
 from time import sleep
 import threading
 
+# 데이터 베이스 업데이트 함수
 def dbUpdater() :
     while True :
         db = DBAccess()
@@ -10,11 +11,13 @@ def dbUpdater() :
 
         sleep(300)
 
+# 차후 추가할 함수
 def test() :
     while True :
         print("This is fun Test")
         sleep(9)
 
+# 스레딩을 이용해여 동시 실행
 if __name__ == '__main__':
     thread_updater = threading.Thread(target=dbUpdater)
 
